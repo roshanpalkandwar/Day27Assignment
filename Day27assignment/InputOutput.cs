40b715d02a42838ea44b40326ef4244fef433bc4
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-10-07 19:42:25 +0530

    [Roshan]UC13-Adress Book

---
+++ Day27assignment/InputOutput.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day27assignment
+{
+    public class InputOutput
+    {
+        public void Read(string path)
+        {
+            using (StreamReader sw = File.OpenText(path))
+            {
+                string line;
+                while ((line = sw.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+        }
+        public void Write(string path)
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("FirstName   : Roshan");
+                sw.WriteLine("LastName    : Palkandwar");
+                sw.WriteLine("Address     : 12 utam Nagar");
+                sw.WriteLine("PhoneNumber : 1234567890");
+                sw.WriteLine("City        : Nagapur");
+                sw.WriteLine("State       : Maharashtra ");
+                sw.WriteLine("Zip         : 345678");
+                sw.WriteLine("Email       : rosh@gmail.com");
+                sw.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+    }
+}
---
+++ Day27assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day27assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            InputOutput readOrWrite = new InputOutput();
+            string path = @"C:\Users\User\Desktop\project assignment\Day27Assignment\Day27Assignment\Day27assignment\AdressBook2.txt";
+            readOrWrite.Read(path);
+            readOrWrite.Write(path);
+            Console.ReadLine();
+        }
+    }
+}
